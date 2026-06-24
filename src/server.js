7cee8510235741d3d3023e001a182dd9ba99f2b7
Amrutha <amruthabhat@gmail.com>
2026-06-24 09:54:03 +0530

    prepared deployment

--- src/server.js
+++ src/server.js
@@ -6,15 +6,17 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/products", productRoutes);
+
 app.get("/health", (req, res) => {
   res.json({
     status: "ok"
   });
 });
 
-app.use("/products", productRoutes);
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
